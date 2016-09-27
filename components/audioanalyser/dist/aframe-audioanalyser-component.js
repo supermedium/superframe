90001740409a2e65f49464d36ed3fe4170e29c8f
Kevin Ngo <me@ngokevin.com>
2016-09-26 19:51:15 -0700

    bump dist

--- components/audioanalyser/dist/aframe-audioanalyser-component.js
+++ components/audioanalyser/dist/aframe-audioanalyser-component.js
@@ -42,13 +42,11 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
 	if (typeof AFRAME === 'undefined') {
 	  throw new Error('Component attempted to register before AFRAME was available.');
 	}
-
-	__webpack_require__(1);
 
 	// Single audio context.
 	var context;
@@ -178,41 +176,6 @@
 	          this.beatCutOff = Math.max(this.beatCutOff, BEAT_MIN);
 	        }
 	      }
-	    }
-	  }
-	});
-
-
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
-
-	/**
-	 * Scale children based on audio frequency levels.
-	 */
-	AFRAME.registerComponent('audioanalyser-levels-scale', {
-	  dependencies: ['audioanalyser'],
-
-	  schema: {
-	    max: {default: 20},
-	    multiplier: {default: 100}
-	  },
-
-	  tick: function (time) {
-	    var children;
-	    var data = this.data;
-	    var levels;
-
-	    levels = this.el.components.audioanalyser.levels;
-	    if (!levels) { return; }
-
-	    children = this.el.children;
-	    for (var i = 0; i < children.length; i++) {
-	      children[i].setAttribute('scale', {
-	        x: 1,
-	        y: Math.min(data.max, Math.max(levels[i] * data.multiplier, 0.05)),
-	        z: 1
-	      });
 	    }
 	  }
 	});
