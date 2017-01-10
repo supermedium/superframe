928563f169068ca7f24f48b9d67bcdf8dd4f2bae
Philip Bell <philip.hunter.bell@gmail.com>
2017-01-09 22:02:54 -0500

    restore dist files

--- components/animation/dist/aframe-animation-component.js
+++ components/animation/dist/aframe-animation-component.js
@@ -181,26 +181,12 @@
 
 	  playAnimation: function () {
 	    if (!this.animation) { return; }
-	    var updateConfig = configDefault;
-	    var propType = getPropertyType(this.el, this.data.property);
-	    if (propType === 'vec2' || propType === 'vec3' || propType === 'vec4') {
-	      updateConfig = configVector;
-	    }
-	    this.config = updateConfig(this.el, this.data, this.config);
-	    this.animation = anime(this.config);
 	    this.animation.restart();
 	    this.animationIsPlaying = true;
 	  },
 
 	  pauseAnimation: function () {
 	    if (!this.animation) { return; }
-	    var updateConfig = configDefault;
-	    var propType = getPropertyType(this.el, this.data.property);
-	    if (propType === 'vec2' || propType === 'vec3' || propType === 'vec4') {
-	      updateConfig = configVector;
-	    }
-	    this.config = updateConfig(this.el, this.data, this.config);
-	    this.animation = anime(this.config);
 	    this.animation.pause();
 	    this.animationIsPlaying = false;
 	  }
