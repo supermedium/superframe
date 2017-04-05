950f9363f9460db1be9b828566b657e80c814e66
Kevin Ngo <me@ngokevin.com>
2017-04-05 14:34:06 -0700

    [architect] drag n drop

--- scenes/architect/components/entity-placer.js
+++ scenes/architect/components/entity-placer.js
@@ -3,29 +3,35 @@ AFRAME.registerComponent('entity-placer', {
     var el = this.el;
     var grabEntity = el.querySelector('#grabSlot');
 
-    el.addEventListener('triggerdown', function (evt) {
+    var workaround = 0;
+    el.addEventListener('primitivedragrelease', function (evt) {
       var newEntity;
       var rotation;
 
-      if (!grabEntity.getAttribute('geometry')) { return; }
+      workaround++;
+      if (workaround % 2 !== 0) { return; }
 
       el.sceneEl.object3D.updateMatrixWorld();
       el.parentEl.object3D.updateMatrixWorld();
       grabEntity.object3D.updateMatrixWorld(true);
       rotation = grabEntity.object3D.getWorldRotation();
 
+      // Create entity by copying selected primitive.
       newEntity = document.createElement('a-entity');
       newEntity.setAttribute('geometry', grabEntity.getAttribute('geometry'), true);
-      newEntity.setAttribute('material', grabEntity.getAttribute('material'), true);
+      newEntity.setAttribute('material',
+        grabEntity.getAttribute('material') || {color: 'red'},
+      true);
       newEntity.setAttribute('position', grabEntity.object3D.getWorldPosition());
       newEntity.setAttribute('rotation', {
         x: THREE.Math.radToDeg(rotation.x),
         y: THREE.Math.radToDeg(rotation.y),
         z: THREE.Math.radToDeg(rotation.z)
       });
-      el.sceneEl.appendChild(newEntity);
 
+      el.sceneEl.appendChild(newEntity);
       el.sceneEl.emit('entityplaced', newEntity);
+      console.log('Primitive placed', newEntity);
     });
   }
 });
--- scenes/architect/components/palette-handler.js
+++ scenes/architect/components/palette-handler.js
@@ -1,18 +1,40 @@
 AFRAME.registerComponent('palette-handler', {
   init: function () {
-    var grabSlot = this.el.querySelector('#grabSlot');
+    var el = this.el;
+    var grabSlot = el.querySelector('#grabSlot');
+    this.hasSelectedPrimitive = false;
 
-    this.el.addEventListener('click', function (evt) {
+    // Select primitive from palette with mousedown.
+    el.addEventListener('mousedown', function (evt) {
       var targetEl = evt.detail.intersectedEl;
 
+      if (!targetEl) { return; }
+
+      // Select primitive.
       if (targetEl.getAttribute('mixin').indexOf('primitive') !== -1) {
         grabSlot.setAttribute('geometry', targetEl.getAttribute('geometry'), true);
         grabSlot.setAttribute('scale', {x: 3, y: 3, z: 3});
       }
 
+      // Select color.
       if (targetEl.getAttribute('mixin').indexOf('color') !== -1) {
         grabSlot.setAttribute('material', targetEl.getAttribute('material'), true);
       }
+
+      this.hasSelectedPrimitive = true;
+    });
+
+    // Release primitive, pass off to entity-placer.
+    el.addEventListener('triggerup', function (evt) {
+      if (!this.hasSelectedPrimitive) { return; }
+      el.emit('primitivedragrelease');
+    });
+
+    // Reset grabSlot once primitive is placed.
+    el.parentNode.addEventListener('entityplaced', function (evt) {
+      this.hasSelectedPrimitive = false;
+      grabSlot.removeAttribute('geometry');
+      grabSlot.removeAttribute('material');
     });
   }
 });
