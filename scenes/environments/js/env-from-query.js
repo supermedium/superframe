be3b81ed5fd29ea389bc6ecc37563c2c192d4ed8
Kevin Ngo <me@ngokevin.com>
2017-06-28 11:28:30 -0700

    work on link/env

---
+++ scenes/environments/js/env-from-query.js
@@ -0,0 +1,25 @@
+// Set environment given query parameter.
+AFRAME.registerComponent('env-from-query', {
+  init: function () {
+    var params = this.getUrlParams();
+    this.el.setAttribute('environment', {
+      preset: params.env || 'forest'
+    });
+  },
+
+  getUrlParams: function () {
+    var match;
+    var pl = /\+/g;  // Regex for replacing addition symbol with a space
+    var search = /([^&=]+)=?([^&]*)/g;
+    var decode = function (s) { return decodeURIComponent(s.replace(pl, ' ')); };
+    var query = window.location.search.substring(1);
+    var urlParams = {};
+
+    match = search.exec(query);
+    while (match) {
+      urlParams[decode(match[1])] = decode(match[2]);
+      match = search.exec(query);
+    }
+    return urlParams;
+  }
+});
---
+++ scenes/environments/js/generate-links.js
@@ -0,0 +1,32 @@
+var LINKS = [
+  {href: '?env=arches', image: 'img/arches.png', title: 'Arches'},
+  {href: '?env=checkboard', image: 'img/checkerboard.png', title: 'Checkerboard'},
+  {href: '?env=egypt', image: 'img/egypt.png', title: 'Egypt'},
+  {href: '?env=forest', image: 'img/forest.png', title: 'Forest'},
+  {href: '?env=goaland', image: 'img/goaland.png', title: 'Goaland'},
+  {href: '?env=goldmine', image: 'img/goldmine.png', title: 'Goldmine'},
+  {href: '?env=japan', image: 'img/japan.png', title: 'Japan'},
+  {href: '?env=poison', image: 'img/poison.png', title: 'Poison'},
+  {href: '?env=threetowers', image: 'img/threetowers.png', title: 'Three Towers'},
+  {href: '?env=tron', image: 'img/tron.png', title: 'Tron'},
+  {href: '?env=yavapai', image: 'img/yavapai.png', title: 'Yavapai'},
+];
+
+AFRAME.registerComponent('generate-links', {
+  init: function () {
+    LINKS.forEach(link => {
+      var linkEl;
+
+      // Current page.
+      if (window.location.search.indexOf(link.href.substring(1)) !== -1) { return; }
+
+      linkEl = document.createElement('a-entity');
+      linkEl.setAttribute('link', {
+        href: link.href,
+        src: link.image,
+        title: link.title
+      });
+      this.el.appendChild(linkEl);
+    });
+  }
+});
