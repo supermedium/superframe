e7b2fc6206d1d1e548744b92f7dccced8029c147
Kevin Ngo <me@ngokevin.com>
2018-05-15 13:49:23 -0700

    [proxy-event] -from- to proxy events from other entities

--- components/proxy-event/dist/aframe-proxy-event-component.js
+++ components/proxy-event/dist/aframe-proxy-event-component.js
@@ -1,110 +1,113 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
+
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-/******/
+
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
+/******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-/******/ 		}
+
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
 /******/ 		};
-/******/
+
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
+
 /******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
+/******/ 		module.loaded = true;
+
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
-/******/
+
+
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-/******/
+
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-/******/
+
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
 /***/ (function(module, exports) {
 
-/**
- * Listen to event and forward to another entity or entities.
- */
-AFRAME.registerComponent('proxy-event', {
-  schema: {
-    captureBubbles: {default: false},
-    event: {type: 'string'},
-    to: {type: 'string'},
-    as: {type: 'string'},
-    bubbles: {default: false}
-  },
+	/**
+	 * Listen to event and forward to another entity or entities.
+	 */
+	AFRAME.registerComponent('proxy-event', {
+	  schema: {
+	    captureBubbles: {default: false},
+	    event: {type: 'string'},
+	    from: {type: 'string'},
+	    to: {type: 'string'},
+	    as: {type: 'string'},
+	    bubbles: {default: false}
+	  },
 
-  multiple: true,
+	  multiple: true,
 
-  init: function () {
-    var data = this.data;
-    var el = this.el;
-    var to;
-    var self = this;
+	  init: function () {
+	    var data = this.data;
+	    var el = this.el;
+	    var from;
+	    var i;
+	    var to;
+	    var self = this;
 
-    if (data.to === 'CHILDREN') {
-      to = el.querySelectorAll('*');
-    } else if (data.to === 'SELF') {
-      to = [el];
-    } else {
-      to = document.querySelectorAll(data.to);
-    }
+	    if (data.from) {
+	      if (data.from === 'PARENT') {
+	        from = [el.parentNode];
+	      } else {
+	        from = document.querySelectorAll(data.from);
+	      }
+	    } else {
+	      if (data.to === 'CHILDREN') {
+	        to = el.querySelectorAll('*');
+	      } else if (data.to === 'SELF') {
+	        to = [el];
+	      } else {
+	        to = document.querySelectorAll(data.to);
+	      }
+	    }
 
-    el.addEventListener(data.event, function (evt) {
-      var data = self.data;
-      var i;
-      if (!data.captureBubbles && evt.target !== el) { return; }
-      for (i = 0; i < to.length; i++) {
-        to[i].emit(data.as || data.event, null, data.bubbles);
-      }
-    });
-  }
-});
+	    if (data.from) {
+	      for (i = 0; i < from.length; i++) {
+	        this.addEventListenerFrom(from[i]);
+	      }
+	    } else {
+	      el.addEventListener(data.event, function (evt) {
+	        var data = self.data;
+	        if (!data.captureBubbles && evt.target !== el) { return; }
+	        for (i = 0; i < to.length; i++) {
+	          to[i].emit(data.as || data.event, evt['detail'] ? evt.detail : null, data.bubbles);
+	        }
+	      });
+	    }
+	  },
+
+	  addEventListenerFrom: function (fromEl) {
+	    var data = this.data;
+	    var self = this;
+	    fromEl.addEventListener(data.event, function (evt) {
+	      var data = self.data;
+	      if (!data.captureBubbles && evt.target !== fromEl) { return; }
+	      self.el.emit(data.as || data.event, evt['detail'] ? evt.detail : null, false);
+	    });
+	  }
+	});
 
 
 /***/ })
--- components/proxy-event/index.js
+++ components/proxy-event/index.js
@@ -5,6 +5,7 @@ AFRAME.registerComponent('proxy-event', {
   schema: {
     captureBubbles: {default: false},
     event: {type: 'string'},
+    from: {type: 'string'},
     to: {type: 'string'},
     as: {type: 'string'},
     bubbles: {default: false}
@@ -15,24 +16,49 @@ AFRAME.registerComponent('proxy-event', {
   init: function () {
     var data = this.data;
     var el = this.el;
+    var from;
+    var i;
     var to;
     var self = this;
 
-    if (data.to === 'CHILDREN') {
-      to = el.querySelectorAll('*');
-    } else if (data.to === 'SELF') {
-      to = [el];
+    if (data.from) {
+      if (data.from === 'PARENT') {
+        from = [el.parentNode];
+      } else {
+        from = document.querySelectorAll(data.from);
+      }
     } else {
-      to = document.querySelectorAll(data.to);
+      if (data.to === 'CHILDREN') {
+        to = el.querySelectorAll('*');
+      } else if (data.to === 'SELF') {
+        to = [el];
+      } else {
+        to = document.querySelectorAll(data.to);
+      }
     }
 
-    el.addEventListener(data.event, function (evt) {
-      var data = self.data;
-      var i;
-      if (!data.captureBubbles && evt.target !== el) { return; }
-      for (i = 0; i < to.length; i++) {
-        to[i].emit(data.as || data.event, evt['detail'] ? evt.detail : null, data.bubbles);
+    if (data.from) {
+      for (i = 0; i < from.length; i++) {
+        this.addEventListenerFrom(from[i]);
       }
+    } else {
+      el.addEventListener(data.event, function (evt) {
+        var data = self.data;
+        if (!data.captureBubbles && evt.target !== el) { return; }
+        for (i = 0; i < to.length; i++) {
+          to[i].emit(data.as || data.event, evt['detail'] ? evt.detail : null, data.bubbles);
+        }
+      });
+    }
+  },
+
+  addEventListenerFrom: function (fromEl) {
+    var data = this.data;
+    var self = this;
+    fromEl.addEventListener(data.event, function (evt) {
+      var data = self.data;
+      if (!data.captureBubbles && evt.target !== fromEl) { return; }
+      self.el.emit(data.as || data.event, evt['detail'] ? evt.detail : null, false);
     });
   }
 });
