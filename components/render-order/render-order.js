0dea6f891ba813d32d35b6de3454f395e4a43ab2
Kevin Ngo <me@ngokevin.com>
2019-05-08 03:05:31 -0700

    [render-order]

---
+++ components/render-order/render-order.js
@@ -0,0 +1,46 @@
+AFRAME.registerSystem('render-order', {
+  schema: {type: 'array'},
+
+  init: function () {
+    this.el.renderer.sortObjects = true;
+  },
+
+  update: function () {
+    this.order = {};
+    for (i = 0; i < this.data.length; i++) {
+      this.order[this.data[i]] = i;
+    }
+  }
+});
+
+AFRAME.registerComponent('render-order', {
+  schema: {type: 'string'},
+
+  init: function () {
+    this.set = this.set.bind(this);
+    this.el.addEventListener('object3dset', evt => {
+      evt.detail.object.traverse(this.set);
+    });
+  },
+
+  update: function () {
+    this.el.object3D.traverse(this.set);
+  },
+
+  set: function (node) {
+    // String (named order).
+    if (isNaN(this.data)) {
+      node.renderOrder = this.system.order[this.data];
+    } else {
+      node.renderOrder = parseFloat(this.data);
+    }
+  }
+});
+
+AFRAME.registerComponent('render-order-recursive', {
+  init: function () {
+    this.el.addEventListener('child-attached', evt => {
+      evt.detail.el.setAttribute('render-order', this.el.getAttribute('render-order'));
+    });
+  }
+});
