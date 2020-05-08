afe98c67f30ec36d98b084c369896a7bfd07bd5d
Kevin Ngo <ktngo09@gmail.com>
2020-05-08 14:52:16 -0700

    bump dists

--- components/haptics/dist/aframe-haptics-component.js
+++ components/haptics/dist/aframe-haptics-component.js
@@ -76,6 +76,12 @@
 
 	    if (this.el.components['tracked-controls'].controller) {
 	      this.gamepad = this.el.components['tracked-controls'].controller;
+
+	      if (this.gamepad.gamepad) {
+	        // WebXR.
+	        this.gamepad = this.gamepad.gamepad;
+	      }
+
 	      if (!this.gamepad || !this.gamepad.hapticActuators ||
 	          !this.gamepad.hapticActuators.length) { return; }
 	      this.addEventListeners();
@@ -83,6 +89,12 @@
 	      this.el.addEventListener('controllerconnected', function init () {
 	        setTimeout(function () {
 	          self.gamepad = self.el.components['tracked-controls'].controller;
+
+	          if (self.gamepad.gamepad) {
+	            // WebXR.
+	            self.gamepad = self.gamepad.gamepad;
+	          }
+
 	          if (!self.gamepad || !self.gamepad.hapticActuators ||
 	              !self.gamepad.hapticActuators.length) { return; }
 	          self.addEventListeners();
