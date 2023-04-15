63814b991f42b9168c76792d1aeb199b25e3b445
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-04-15 16:25:46 +0100

    Update to work with latest code

--- components/orbit-controls/index.js
+++ components/orbit-controls/index.js
@@ -1,4 +1,5 @@
-require('./lib/OrbitControls');
+import { OrbitControls } from './lib/OrbitControls.js';
+THREE.OrbitControls = OrbitControls
 
 var bind = AFRAME.utils.bind;
 
@@ -110,12 +111,12 @@ AFRAME.registerComponent('orbit-controls', {
     controls.enableRotate = data.enableRotate;
     controls.enableZoom = data.enableZoom;
     controls.keyPanSpeed = data.keyPanSpeed;
-    controls.maxPolarAngle = THREE.Math.degToRad(data.maxPolarAngle);
-    controls.maxAzimuthAngle = THREE.Math.degToRad(data.maxAzimuthAngle);
+    controls.maxPolarAngle = THREE.MathUtils.degToRad(data.maxPolarAngle);
+    controls.maxAzimuthAngle = THREE.MathUtils.degToRad(data.maxAzimuthAngle);
     controls.maxDistance = data.maxDistance;
     controls.minDistance = data.minDistance;
-    controls.minPolarAngle = THREE.Math.degToRad(data.minPolarAngle);
-    controls.minAzimuthAngle = THREE.Math.degToRad(data.minAzimuthAngle);
+    controls.minPolarAngle = THREE.MathUtils.degToRad(data.minPolarAngle);
+    controls.minAzimuthAngle = THREE.MathUtils.degToRad(data.minAzimuthAngle);
     controls.minZoom = data.minZoom;
     controls.panSpeed = data.panSpeed;
     controls.rotateSpeed = data.rotateSpeed;
