cfd7794fd681b16d45b09ba1abe780d9e44fe282
Schweik7 <weibidewo0@gmail.com>
2024-03-25 21:55:56 +0800

    text-animation-component and reusing fonts of text-geometry

--- components/text-geometry/index.js
+++ components/text-geometry/index.js
@@ -3,25 +3,22 @@
  */
 require('./lib/FontLoader')
 require('./lib/TextGeometry')
-
 var debug = AFRAME.utils.debug;
-
 var error = debug('aframe-text-component:error');
-
 var fontLoader = new THREE.FontLoader();
-
+var fontCache = {}; // cache for loaded fonts
 AFRAME.registerComponent('text-geometry', {
   schema: {
-    bevelEnabled: {default: false},
-    bevelSize: {default: 8, min: 0},
-    bevelThickness: {default: 12, min: 0},
-    curveSegments: {default: 12, min: 0},
-    font: {type: 'asset', default: 'https://rawgit.com/ngokevin/kframe/master/components/text-geometry/lib/helvetiker_regular.typeface.json'},
-    height: {default: 0.05, min: 0},
-    size: {default: 0.5, min: 0},
-    style: {default: 'normal', oneOf: ['normal', 'italics']},
-    weight: {default: 'normal', oneOf: ['normal', 'bold']},
-    value: {default: ''}
+    bevelEnabled: { default: false },
+    bevelSize: { default: 8, min: 0 },
+    bevelThickness: { default: 12, min: 0 },
+    curveSegments: { default: 12, min: 0 },
+    font: { type: 'asset', default: 'https://rawgit.com/ngokevin/kframe/master/components/text-geometry/lib/helvetiker_regular.typeface.json' },
+    height: { default: 0.05, min: 0 },
+    size: { default: 0.5, min: 0 },
+    style: { default: 'normal', oneOf: ['normal', 'italics'] },
+    weight: { default: 'normal', oneOf: ['normal', 'bold'] },
+    value: { default: '' }
   },
 
   /**
@@ -32,19 +29,29 @@ AFRAME.registerComponent('text-geometry', {
     var data = this.data;
     var el = this.el;
 
-    var mesh = el.getOrCreateObject3D('mesh', THREE.Mesh);
-    if (data.font.constructor === String) {
-      // Load typeface.json font.
-      fontLoader.load(data.font, function (response) {
-        var textData = AFRAME.utils.clone(data);
-        textData.font = response;
-        mesh.geometry = new THREE.TextGeometry(data.value, textData);
-      });
-    } else if (data.font.constructor === Object) {
-      // Set font if already have a typeface.json through setAttribute.
-      mesh.geometry = new THREE.TextGeometry(data.value, data);
+    // check if font is already cached
+    if (fontCache[data.font]) {
+      this.createTextGeometry(fontCache[data.font]);
     } else {
-      error('Must provide `font` (typeface.json) or `fontPath` (string) to text component.');
+      if (data.font.constructor === String) {
+        fontLoader.load(data.font, (response) => {
+          fontCache[data.font] = response; // cache font
+          this.createTextGeometry(response);
+        });
+      } else if (data.font.constructor === Object) {
+        this.createTextGeometry(data.font);
+      } else {
+        error('Must provide `font` (typeface.json) or `fontPath` (string) to text component.');
+      }
     }
+  },
+
+  createTextGeometry: function (font) {
+    var data = this.data;
+    var el = this.el;
+
+    var textData = AFRAME.utils.clone(data);
+    textData.font = font;
+    el.getOrCreateObject3D('mesh', THREE.Mesh).geometry = new THREE.TextGeometry(data.value, textData);
   }
 });
